c2121c283837593abb8801c7805416d5e3876c5d
phara0hcom <t.elsay3d@gmail.com>
2020-11-21 17:42:40 +0900

    no import react for JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './container/Home';
 import Other from './container/Other';
--- src/react-env.d.ts
+++ src/react-env.d.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference types="node" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
