6354cdf425e262cad13d1f88dea8203f1348d5a7
phara0hcom <t.elsay3d@gmail.com>
2020-12-27 15:42:15 +0900

    add NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { Route, Switch } from 'react-router-dom';
+import TopBar from './components/topBar/TopBar';
 import Home from './container/Home';
 import Other from './container/Other';
 
-const App: React.FC = () => {
-  return (
-    <div className="container">
-      <Switch>
-        <Route path="/other">
-          <Other />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </div>
-  );
-};
+const App: React.FC = () => (
+  <div className="container">
+    <TopBar />
+    <Switch>
+      <Route path="/other">
+        <Other />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </div>
+);
 
 export default App;
